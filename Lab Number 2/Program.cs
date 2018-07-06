38201c4575774230a1a4843595bac5ff91993d8e
noelle none <noe.ililonga@gmail.com>
2018-07-05 20:39:43 -0400

    extended challenges completed

--- Lab Number 2/Program.cs
+++ Lab Number 2/Program.cs
@@ -14,13 +14,15 @@ namespace Lab_Number_2
             do
             {
                 //declare variable
-                double input1, input2;
+                double input1, input2, input3;
                 
                 //user enters values
                 Console.WriteLine("Please enter length");
                 input1 = double.Parse(Console.ReadLine());
                 Console.WriteLine("Please enter width");
                 input2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter height");
+                input3 = double.Parse(Console.ReadLine()); 
 
                 //calculate to find the area
                 Console.WriteLine($"The area of your room is: {(input1 * input2)}");
@@ -28,7 +30,11 @@ namespace Lab_Number_2
                 //calculate to find perimeter
                 Console.WriteLine($"The perimeter of your room is:{(2 * input1) + (2 * input2)}");
 
+                //calculate volume of the room 
+                Console.WriteLine($"The volume of the room is:{(input1 * input2 * input3)}");
+
                 //ask user if they would like to continue
+
 
                 Console.WriteLine("Would you like to continue? (y/n)");
                 answer = Console.ReadLine();
